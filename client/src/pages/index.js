43fda8a1f5f525f24932e22984f5745b5ab7bb8b
Tyler Williams <tsw38@cornell.edu>
2021-04-22 15:40:55 -0500

    remove ACF

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -12,14 +12,14 @@ const Home = () => {
       </Head>
 
       <Box
+        h="100%"
         as="main"
         bg="#1a221b"
         display="flex"
+        color="#e0d6c6"
+        alignItems="center"
         flexDirection="column"
         justifyContent="center"
-        alignItems="center"
-        h="100%"
-        color="#e0d6c6"
       >
         <Heading as="h1" fontSize="2xl">
           ty.lerscott.com
