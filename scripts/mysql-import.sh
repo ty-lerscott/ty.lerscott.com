e6537792ea131754de2bb6ba18ea807a52acb86e
Tyler Williams <tsw38@cornell.edu>
2021-04-25 22:12:01 -0500

    ignore

--- scripts/mysql-import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export $(egrep -v '^#' ./client/.env.local | xargs)
-
-echo "UPLOADING to $DB_NAME..."
-
-# drop all the tables in the database
-mysql -h$DB_HOST -u$DB_USER -p$DB_PASS -D $DB_NAME < $DB_BACKUP
-
-echo "CONTENTS HAVE BEEN UPDATED TO $DB_NAME"
--- scripts/mysql-truncate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export $(egrep -v '^#' ./client/.env.local | xargs)
-
-echo "DROPPING $DB_NAME..."
-
-# drop all the tables in the database
-for i in `mysql -h$DB_HOST -u$DB_USER -p$DB_PASS $DB_NAME -e "show tables" | grep -v Tables_in` ; do  echo $i && mysql -h$DB_HOST -u$DB_USER -p$DB_PASS $DB_NAME -e "SET FOREIGN_KEY_CHECKS = 0; drop table $i ; SET FOREIGN_KEY_CHECKS = 1" ; done
-
-echo "$DB_NAME - $i HAS BEEN DROPPED"
